75ad01a07fed6782faf60ff0853affe4a27e648c
Surabhi Rani <rani2001surabhi@gmail.com>
2021-09-08 09:38:50 +0530

    Update covi.js

--- covi.js
+++ covi.js
@@ -22,7 +22,19 @@ $(document).ready(function(){
         $('.scroll-top').hide();
       }
   
-    
+       $('section').each(function(){
+  
+        let top = $(window).scrollTop();
+        let offset = $(this).offset().top - 200;
+        let id = $(this).attr('id');
+        let height = $(this).height();
+  
+        if(top > offset && top < offset + height){
+          $('.navbar a').removeClass('active');
+          $('.navbar').find(`[href="#${id}"]`).addClass('active');
+        }
+  
+      });
     });
   
     // smooth scrolling 
